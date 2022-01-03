d75868242347c221aeb4a6ae08fdad43c45bcd49
david83748 <david.vashchenko@gmail.com>
2022-01-03 14:30:48 +0200

    created new loop

---
+++ main.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 8; i++) {
+    console.log(i);
+  }
+  
+  let elementDo = 0;
+  do {
+    console.log(elementDo);
+    elementDo++;
+  } while (elementDo <= 6);
+  
+  let elementCount = 1;
+  while (elementCount <= 8) {
+    console.log(elementCount);
+    elementCount++;
+  }
